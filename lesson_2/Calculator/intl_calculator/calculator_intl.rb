2fab7d88e0166d7d96bc4d0b95aaee295db3e25d
James Wallace <4538317+NZLWally@users.noreply.github.com>
2020-12-02 23:01:34 +1300

    Changed `LANG` to `lang` and modified method of updating `lang` created `access_l2` and `l3` and updated accessing `MSGS` Updated `play_again?`

--- lesson_2/Calculator/intl_calculator/calculator_intl.rb
+++ lesson_2/Calculator/intl_calculator/calculator_intl.rb
@@ -7,7 +7,17 @@ LANGS = YAML.load_file('languages.yml')
 OPS = YAML.load_file('operations.yml')
 MSGS = YAML.load_file('messages.yml')
 
-# ------------------- Methods --------------------------------------------------
+# ------------------- Access MSGS ----------------------------------------------
+
+def access_l2(lang, msg_type, msg)
+  MSGS[lang][msg_type][msg]
+end
+
+def access_l3(lang, msg_type, sub_msg_type, msg)
+  MSGS[lang][msg_type][sub_msg_type][msg]
+end
+
+# ------------------- Display Methods ------------------------------------------
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -19,9 +29,51 @@ end
 
 def display_langs
   LANGS.each do |key, lang|
-    puts "'#{key}' / '#{lang}'"
+    puts "   '#{key}' / '#{lang}'"
   end
 end
+
+def display_result(lang, first, second, calc_result, operation)
+  first = format_thousands(lang, first)
+  second = format_thousands(lang, second)
+  calc_result = format_thousands(lang, calc_result)
+  prompt result_message(lang, first, second, calc_result, operation)
+end
+
+def format_thousands(lang, input)
+  if input == access_l2(lang, 'invalid_inputs', 'zero_divisor') || input < 1000
+    input
+  else
+    left, right = input.to_s.split('.')
+
+    left = left.chars
+    increments = []
+    while left.size > 0
+      increments.unshift(left.pop(3).join)
+    end
+
+    left = increments.join(',')
+    right.nil? ? left : left << '.' << right
+  end
+end
+
+def result_message(lang, first, second, calc_result, operation)
+  operation_number = OPS[lang].key(operation)
+  default_lang = LANGS.keys.first
+  english_op = OPS[default_lang][operation_number]
+  if calc_result == access_l2(lang, 'invalid_inputs', 'zero_divisor')
+    "#{first} #{access_l3(lang, 'ops', english_op, 'msg')} " \
+    "#{second} #{calc_result}"
+  elsif operation == OPS[lang]['2']
+    "#{second} #{access_l3(lang, 'ops', english_op, 'msg')} " \
+    "#{first} equals #{calc_result}"
+  else
+    "#{first} #{access_l3(lang, 'ops', english_op, 'msg')} " \
+    "#{second} equals #{calc_result}"
+  end
+end
+
+# ------------------- Validation Methods ---------------------------------------
 
 def valid_lang?(lang)
   LANGS.keys.include?(lang.downcase) ||
@@ -40,92 +92,118 @@ def valid_float?(input)
   !!(/\d/.match(input) && /^-?\d*.?\d*$/.match(input) && !input.empty?)
 end
 
-def valid_operation?(input)
-  !!(OPS[LANG].keys.include?(input) ||
-    OPS[LANG].values.include?(input.downcase))
+def valid_operation?(lang, input)
+  !!(OPS[lang].keys.include?(input) ||
+    OPS[lang].values.include?(input.downcase))
 end
 
-def update_lang?
-  prompt MSGS[LANG]['requests']['lang']
-  update = ''
+def valid_update?(input)
+  AFFIRM_DEFAULT.include?(input) || NEGATIVE_DEFAULT.include?(input)
+end
+
+def valid_exit?(input)
+  AFFIRM_LANG.include?(input) || NEGATIVE_LANG.include?(input)
+end
+
+# ------------------- Language Methods -----------------------------------------
+
+def normalise_lang(lang)
+  if update_lang?(lang)
+    lang = request_lang(lang)
+    if LANGS.values.include?(lang.capitalize)
+      LANGS.key(lang.capitalize)
+    else
+      lang.downcase
+    end
+  else
+    lang
+  end
+end
+
+def update_lang?(lang)
+  prompt access_l2(lang, 'requests', 'lang')
+  update = nil
   loop do
     update = gets.chomp.downcase
-    break if valid_exit?(repeat)
+    break if valid_update?(update)
 
-    prompt MSGS[LANG]['invalid_inputs']['repeat']
+    prompt access_l2(lang, 'invalid_inputs', 'repeat')
   end
-
-  if AFFIRMATIVE.include?(repeat)
-    true
-  elsif NEGATIVE.include?(repeat)
-    false
-  end
+  AFFIRM_DEFAULT.include?(update)
 end
 
-def request_lang
-  lang = 'en'
+def request_lang(lang)
+  new_lang = nil
   loop do
-    prompt MSGS['en']['requests']['lang']
+    prompt access_l2(lang, 'requests', 'choices')
     display_langs
-    lang = gets.chomp
-    break if valid_lang?(lang)
-    prompt MSGS['en']['invalid_inputs']['lang']
+    new_lang = gets.chomp
+    break if valid_lang?(new_lang)
+    prompt access_l2(lang, 'invalid_inputs', 'lang')
   end
-  lang
+  new_lang
 end
 
-def normalise_lang
-  lang = request_lang
-  if LANGS.values.include?(lang.capitalize)
-    LANGS.key(lang.capitalize)
-  else
-    lang.downcase
-  end
-end
+# ------------------- Request Methods ------------------------------------------
 
-def request_name
-  prompt MSGS[LANG]['messages']['welcome']
+def request_name(lang)
+  prompt access_l2(lang, 'messages', 'welcome')
 
   loop do
-    prompt MSGS[LANG]['requests']['name']
+    prompt access_l2(lang, 'requests', 'name')
     name = gets.chomp
     break name if valid_name?(name)
 
-    prompt MSGS[LANG]['invalid_inputs']['name']
+    prompt access_l2(lang, 'invalid_inputs', 'name')
   end
 end
 
-def request_number(msg)
+def request_number(lang, msg)
   prompt msg
   loop do
     input = gets.chomp.downcase
     break input.to_i if valid_integer?(input)
     break input.to_f if valid_float?(input)
-    prompt MSGS[LANG]['invalid_inputs']['integer']
+    prompt access_l2(lang, 'invalid_inputs', 'number')
   end
 end
 
-def request_operation(msg)
+def request_operation(lang, msg)
   prompt msg
   loop do
     input = gets.chomp.downcase
-    break input if valid_operation?(input)
-    prompt MSGS[LANG]['invalid_inputs']['operation']
+    break input if valid_operation?(lang, input)
+    prompt access_l2(lang, 'invalid_inputs', 'operation')
   end
 end
 
-def normalise_operation(msg)
-  input = request_operation(msg)
-  if OPS[LANG].keys.include?(input)
-    OPS[LANG][input]
+def normalise_operation(lang, msg)
+  input = request_operation(lang, msg)
+  if OPS[lang].keys.include?(input)
+    OPS[lang][input]
   else
     input
   end
 end
 
-def divide(first, second)
+def play_again?(lang)
+  prompt access_l2(lang, 'requests', 'repeat')
+  repeat = nil
+  loop do
+    repeat = gets.chomp.downcase
+    break if valid_exit?(repeat)
+
+    prompt access_l2(lang, 'invalid_inputs', 'repeat')
+  end
+
+  AFFIRM_LANG.include?(repeat)
+end
+
+# ------------------- Calculation Methods --------------------------------------
+
+def divide(lang, first, second)
   if second.zero?
-    MSGS[LANG]['invalid_inputs']['zero_divisor']
+    access_l2(lang, 'invalid_inputs', 'zero_divisor')
   elsif (first / second).to_f != first / second.to_f
     first / second.to_f
   else
@@ -133,123 +211,55 @@ def divide(first, second)
   end
 end
 
-def calculate_result(first, second, operation)
+def calculate_result(lang, first, second, operation)
   case operation
-  when MSGS[LANG]['operations']['add']['name'] then first + second
-  when MSGS[LANG]['operations']['subtract']['name'] then first - second
-  when MSGS[LANG]['operations']['multiply']['name'] then first * second
-  when MSGS[LANG]['operations']['divide']['name'] then divide(first, second)
+  when access_l3(lang, 'ops', 'add', 'name') then first + second
+  when access_l3(lang, 'ops', 'subtract', 'name') then first - second
+  when access_l3(lang, 'ops', 'multiply', 'name') then first * second
+  when access_l3(lang, 'ops', 'divide', 'name') then divide(lang, first, second)
   end
-end
-
-def display_result(first, second, calc_result, operation)
-  first = format_thousands(first)
-  second = format_thousands(second)
-  calc_result = format_thousands(calc_result)
-  prompt result_message(first, second, calc_result, operation)
-end
-
-def format_thousands(input)
-  if input == MSGS[LANG]['invalid_inputs']['zero_divisor'] || input < 1000
-    input
-  else
-    left, right = input.to_s.split('.')
-
-    left = left.chars
-    increments = []
-    while left.size > 0
-      increments.unshift(left.pop(3).join)
-    end
-
-    left = increments.join(',')
-    right.nil? ? left : left << '.' << right
-  end
-end
-
-def result_message(first, second, calc_result, operation)
-  operation_number = OPS[LANG].key(operation)
-  english_operation = OPS['en'][operation_number]
-  if calc_result == MSGS[LANG]['invalid_inputs']['zero_divisor']
-    "#{first} #{MSGS[LANG]['operations'][english_operation]['msg']} " \
-    "#{second} #{calc_result}"
-  elsif operation == OPS[LANG]['2']
-    "#{second} #{MSGS[LANG]['operations'][english_operation]['msg']} " \
-    "#{first} equals #{calc_result}"
-  else
-    "#{first} #{MSGS[LANG]['operations'][english_operation]['msg']} " \
-    "#{second} equals #{calc_result}"
-  end
-end
-
-def play_again?
-  prompt MSGS[LANG]['requests']['repeat']
-  repeat = ''
-  loop do
-    repeat = gets.chomp.downcase
-    break if valid_exit?(repeat)
-
-    prompt MSGS[LANG]['invalid_inputs']['repeat']
-  end
-
-  if AFFIRMATIVE.include?(repeat)
-    true
-  elsif NEGATIVE.include?(repeat)
-    false
-  end
-end
-
-def valid_exit?(input)
-  AFFIRMATIVE.include?(input) || NEGATIVE.include?(input)
 end
 
 # ------------------- Constants ------------------------------------------------
 
-# lang = 'en'
+clear_screen
+default_lang = LANGS.keys.first
+
+AFFIRM_DEFAULT = [access_l2(default_lang, 'responses', 'affirmative_short'),
+                  access_l2(default_lang, 'responses', 'affirmative_long')]
+
+NEGATIVE_DEFAULT = [access_l2(default_lang, 'responses', 'negative_short'),
+                    access_l2(default_lang, 'responses', 'negative_long')]
+
+lang = normalise_lang(default_lang)
+
+AFFIRM_LANG = [access_l2(lang, 'responses', 'affirmative_short'),
+               access_l2(lang, 'responses', 'affirmative_long')]
+
+NEGATIVE_LANG = [access_l2(lang, 'responses', 'negative_short'),
+                 access_l2(lang, 'responses', 'negative_long')]
+
+# ------------------- Main Program----------------------------------------------
 
 clear_screen
-LANG = normalise_lang
-
-AFFIRMATIVE = [MSGS[LANG]['responses']['affirmative_short'],
-          MSGS[LANG]['responses']['affirmative_long']]
-
-NEGATIVE = [MSGS[LANG]['responses']['negative_short'],
-        MSGS[LANG]['responses']['negative_long']]
-
-# ------------------- Program --------------------------------------------------
-
-# clear_screen
-# lang = normalise_lang
+name = request_name(lang)
 
 clear_screen
-name = request_name
-
-clear_screen
-prompt "#{MSGS[LANG]['messages']['greeting']}, #{name}!"
+prompt "#{access_l2(lang, 'messages', 'greeting')}, #{name}!"
 
 loop do
-  first_num = request_number(MSGS[LANG]['requests']['first_number'])
-  second_num = request_number(MSGS[LANG]['requests']['second_number'])
-  operation = normalise_operation(MSGS[LANG]['requests']['operator'])
-  calc_result = calculate_result(first_num, second_num, operation)
+  first_num = request_number(lang, access_l2(lang, 'requests', 'first_num'))
+  second_num = request_number(lang, access_l2(lang, 'requests', 'second_num'))
+  operation = normalise_operation(lang, access_l2(lang, 'requests', 'operator'))
+  calc_result = calculate_result(lang, first_num, second_num, operation)
   clear_screen
-  display_result(first_num, second_num, calc_result, operation)
+  display_result(lang, first_num, second_num, calc_result, operation)
 
-  break unless play_again?
+  break unless play_again?(lang)
   clear_screen
-  prompt "#{MSGS[LANG]['messages']['repeat_welcome']}, #{name}!"
+  prompt "#{access_l2(lang, 'messages', 'repeat_welcome')}, #{name}!"
 end
 
 clear_screen
-prompt "#{MSGS[LANG]['messages']['exit']}, #{name}. " \
-        "#{MSGS[LANG]['messages']['signoff']}"
-
-# changing LANG to lang default value = 'en' then re-assign with request method
-
-# change prompt
-  # def prompt(lang, msg_type, msg)
-  #   puts "=> #{MSGS[lang][msg_type][msg]}"
-  # end
-
-# update play_again?
-  # AFFIRMATIVE.include?(repeat)
-
+prompt "#{access_l2(lang, 'messages', 'exit')}, #{name}. " \
+       "#{access_l2(lang, 'messages', 'signoff')}"
