ffe4d069d986a32e7938addac3263fe91cda727e
James Wallace <4538317+james-wallace-nz@users.noreply.github.com>
2021-09-07 21:16:01 +1200

    Study guide questions

---
+++ small_problems/study_guide.rb
@@ -0,0 +1,10 @@
+# "reverse an array without using the built-in reverse method"
+
+
+# "select the element out of the array if its index is a fibonacci number"
+
+
+# "write a method to determine if a word is a palindrome, without using the reverse method"
+
+
+# Write a method that takes a string argument and returns a new string that contains the value of the original string with all consecutive duplicate characters collapsed into a single character.
